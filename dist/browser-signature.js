d1d7d008021b5d9cec522a8b633d931b1e390d0d
si delv <151720755+creuserr@users.noreply.github.com>
2024-06-08 00:32:06 +0800

    update browser-signature.js

--- dist/browser-signature.js
+++ dist/browser-signature.js
@@ -1,19 +1,57 @@
-function getBrowserSignature() {
-  if(this.window == undefined) throw Error("Cannot obtain the browser signature in a non-client-sided program")
-  var n = window.navigator
-  var s = window.screen
-  var sw = [n.appName, n.appCodeName, n.product, n.productSub, n.vendor]
-  var hw = [s.availHeight, s.availWidth, s.pixelDepth, s.colorDepth, n.hardwareConcurrency, n.maxTouchPoints, window.devicePixelRatio]
-  var sp = ["WebGL2RenderingContext", "Worker", "WebSocket", "WebAssembly", "RTCCertificate", "IDBDatabase"]
-  function enc(raw) {
-    return hash(new Array(Math.ceil(raw.length / 2)).fill(0).map((_, i) => {
-    return pair(raw.charCodeAt(i * 2), raw.charCodeAt(i * 2 + 1) || 0)
-    }).join("")).toString(16)
+($ => {
+
+function getBrowserSignature(conf) {
+  const config = conf || {}
+  const kit = config.kit || {
+    global: globalThis.window,
+    hash: getBrowserSignature.hash
   }
+  if(kit == null) throw new Error("No provided kit")
+  const n = kit.global.navigator
+  const s = kit.global.screen
+  let sw = [n.appName, n.appCodeName, n.product, n.productSub, n.vendor]
+  if(Array.isArray(config.software)) {
+    if(config.override == true) {
+      sw = config.software
+    } else {
+      sw = sw.concat(config.software)
+    }
+  }
+  let hw = [s.availHeight, s.availWidth, s.pixelDepth, s.colorDepth, n.hardwareConcurrency, n.maxTouchPoints, kit.devicePixelRatio]
+  if(Array.isArray(config.hardware)) {
+    if(config.override == true) {
+      hw = config.hardware
+    } else {
+      hw = hw.concat(config.hardware)
+    }
+  }
+  let comp = ["WebGL2RenderingContext", "Worker", "WebSocket", "WebAssembly", "RTCCertificate", "IDBDatabase"]
+  if(Array.isArray(config.compatibility)) {
+    if(config.override == true) {
+      comp = config.compatibility
+    } else {
+      comp = comp.concat(config.compatibility)
+    }
+  }
+  const sign = {
+    software: kit.hash(sw.join("")),
+    hardware: kit.hash(hw.join("")),
+    compatibility: kit.hash(comp.map(i => i in kit.global ? 1 : 0).join(""))
+  }
+  sign.signature = {
+    all: kit.hash(sign.software + sign.hardware + sign.compatibility),
+    softhard: kit.hash(sign.software + sign.hardware),
+    hardcomp: kit.hash(sign.hardware + sign.compatibility),
+    softcomp: kit.hash(sign.software + sign.compatibility)
+  }
+  return sign
+}
+
+getBrowserSignature.hash = (raw) => {
   function hash(str) {
-    var h1 = 0xdeadbeef ^ 0
-    var h2 = 0x41c6ce57 ^ 0
-    for(var i = 0; i < str.length; i++) {
+    let h1 = 0xdeadbeef ^ 0
+    let h2 = 0x41c6ce57 ^ 0
+    for(let i = 0; i < str.length; i++) {
     var ch = str.charCodeAt(i)
     h1 = Math.imul(h1 ^ ch, 2654435761)
     h2 = Math.imul(h2 ^ ch, 1597334677)
@@ -27,16 +65,13 @@ function getBrowserSignature() {
   function pair(a, b) {
     return 0.5 * (a + b) * (a + b + 1) + b
   }
-  var sign = {
-    software: enc(sw.join("")),
-    hardware: enc(hw.join("")),
-    compatibility: enc(sp.map(i => i in window ? 1 : 0).join(""))
-  }
-  sign.signature = {
-    all: enc(sign.software + sign.hardware + sign.compatibility),
-    softhard: enc(sign.software + sign.hardware),
-    hardcomp: enc(sign.hardware + sign.compatibility),
-    softcomp: enc(sign.software + sign.compatibility)
-  }
-  return sign
+  return hash(new Array(Math.ceil(raw.length / 2)).fill(0).map((_, i) => {
+    return pair(raw.charCodeAt(i * 2), raw.charCodeAt(i * 2 + 1) || 0)
+  }).join("")).toString(16)
 }
+
+if("module" in $) $.module.exports = getBrowserSignature;
+else $.getBrowserSignature = getBrowserSignature
+})(globalThis)
+
+console.log(getBrowserSignature());
